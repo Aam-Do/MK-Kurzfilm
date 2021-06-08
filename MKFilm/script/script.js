3eb6936939f38d57c081edf2ba268d7cb5a0b598
Am.D <ameliedelloro@gmail.com>
2021-06-08 11:52:35 +0200

    buttonswork

--- MKFilm/script/script.js
+++ MKFilm/script/script.js
@@ -1,1 +1,46 @@
+var buttonDiv;
+var allButtons = [
+    { id: 0, content: "option0" },
+    { id: 1, content: "option1" }
+];
+var buttons;
+window.addEventListener("load", function () { load(); });
+function load() {
+    buttonDiv = document.querySelector("#buttonDiv");
+    drawButtons();
+    console.log("hi");
+}
+function drawButtons() {
+    buttonDiv.innerHTML = "";
+    var _loop_1 = function (i) {
+        var button = allButtons[i];
+        var newButton = document.createElement("button");
+        var node = document.createTextNode(button.content);
+        var buttonId = document.createAttribute("id");
+        buttonId.value = (button.id).toString();
+        newButton.appendChild(node);
+        newButton.setAttributeNode(buttonId);
+        buttonDiv.appendChild(newButton);
+        newButton.addEventListener("click", function () { buttonClick(button.id); });
+    };
+    for (var i = 0; i < allButtons.length; i++) {
+        _loop_1(i);
+    }
+}
+function buttonClick(id) {
+    for (var i = 0; i < allButtons.length; i++) {
+        var button = allButtons[i];
+        if (button.id == id) {
+            console.log("option " + id + " was clicked");
+            allButtons.length = 0;
+            if (id == 0 || id == 1) {
+                allButtons.push({ id: 2, content: "option2" }, { id: 3, content: "option3" });
+            }
+            else if (id == 2) {
+                allButtons.push({ id: 4, content: "option4" }, { id: 5, content: "option5" });
+            }
+        }
+    }
+    drawButtons();
+}
 //# sourceMappingURL=script.js.map
--- MKFilm/script/script.ts
+++ MKFilm/script/script.ts
@@ -1,13 +1,52 @@
-window.addEventListener("load", function (): void {
-    let allOptions: number[] = [1, 2];
-    let buttons: HTMLElement;
-    buttonEventListeners();
+var buttonDiv: HTMLElement;
+interface OptionButtons { id: number; content: string; }
+let allButtons: OptionButtons[] = [
+    {id: 0, content: "option0"},
+    {id: 1, content: "option1"}
+];
+var buttons: HTMLElement;
+window.addEventListener("load", function(): void { load(); });
+
+function load(): void {
+    buttonDiv = document.querySelector("#buttonDiv");
+    drawButtons();
     console.log("hi");
+}
+function drawButtons(): void {
+    buttonDiv.innerHTML = "";
 
-    function buttonEventListeners (): void {
-        for (let i: number = 1; i <= allOptions.length; i++)
-        //  buttons = document.querySelector("")
-        console.log("i " + allOptions);
+    for (let i: number = 0; i < allButtons.length; i++) {
+        let button: OptionButtons = allButtons[i];
+
+        let newButton: HTMLButtonElement = document.createElement("button");
+        let node: Node = document.createTextNode(button.content);
+        let buttonId: Attr = document.createAttribute("id");
+
+        buttonId.value = (button.id).toString();
+
+        newButton.appendChild(node);
+        newButton.setAttributeNode(buttonId);
+
+        buttonDiv.appendChild(newButton);
+
+        newButton.addEventListener("click", function (): void {buttonClick(button.id); });
     }
+}
+function buttonClick (id: number): void {
+    for (let i: number = 0; i < allButtons.length; i++) {
+        let button: OptionButtons = allButtons[i];
+        
+        if (button.id == id) {
+            console.log("option " + id + " was clicked");
+            allButtons.length = 0;
+            if (id == 0 || id == 1) {
+                allButtons.push({id: 2, content: "option2"}, {id: 3, content: "option3"});
+            }
+            else if (id == 2) {
+                allButtons.push({id: 4, content: "option4"}, {id: 5, content: "option5"});
+            }
+        }
+    }
+    drawButtons();
+}
 
-});
