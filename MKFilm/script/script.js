f23f570f5ab31a087ff9fd142e722e0c2117c778
Am.D <ameliedelloro@gmail.com>
2021-06-10 13:58:28 +0200

    addedalloptions

--- MKFilm/script/script.js
+++ MKFilm/script/script.js
@@ -1,7 +1,7 @@
 var buttonDiv;
 var allButtons = [
-    { id: 0, content: "option0" },
-    { id: 1, content: "option1" }
+    { id: 0, content: "Tee" },
+    { id: 1, content: "Apfelsaft" }
 ];
 var buttons;
 window.addEventListener("load", function () { load(); });
@@ -34,10 +34,25 @@ function buttonClick(id) {
             console.log("option " + id + " was clicked");
             allButtons.length = 0;
             if (id == 0 || id == 1) {
-                allButtons.push({ id: 2, content: "option2" }, { id: 3, content: "option3" });
+                allButtons.push({ id: 2, content: "Spazieren gehen" }, { id: 3, content: "Zuhause bleiben" });
             }
-            else if (id == 2) {
-                allButtons.push({ id: 4, content: "option4" }, { id: 5, content: "option5" });
+            else if (id == 2 || id == 9) {
+                allButtons.push({ id: 4, content: "Auf dem Weg bleiben" }, { id: 5, content: "Rascheln nachgehen" });
+            }
+            else if (id == 3) {
+                allButtons.push({ id: 6, content: "Herbitten" }, { id: 7, content: "Nicht herbitten" });
+            }
+            else if (id == 7) {
+                allButtons.push({ id: 8, content: "Zuhause bleiben" }, { id: 9, content: "Nachsehen" });
+            }
+            else if (id == 8) {
+                allButtons.push({ id: 10, content: "Netflix Serie" }, { id: 11, content: "Disney+ Film" });
+            }
+            else if (id == 12) {
+                allButtons.push({ id: 0, content: "Tee" }, { id: 1, content: "Apfelsaft" });
+            }
+            else {
+                allButtons.push({ id: 12, content: "Restart" });
             }
         }
     }
--- MKFilm/script/script.ts
+++ MKFilm/script/script.ts
@@ -1,8 +1,8 @@
 var buttonDiv: HTMLElement;
 interface OptionButtons { id: number; content: string; }
 let allButtons: OptionButtons[] = [
-    {id: 0, content: "option0"},
-    {id: 1, content: "option1"}
+    {id: 0, content: "Tee"},
+    {id: 1, content: "Apfelsaft"}
 ];
 var buttons: HTMLElement;
 window.addEventListener("load", function(): void { load(); });
@@ -40,10 +40,25 @@ function buttonClick (id: number): void {
             console.log("option " + id + " was clicked");
             allButtons.length = 0;
             if (id == 0 || id == 1) {
-                allButtons.push({id: 2, content: "option2"}, {id: 3, content: "option3"});
+                allButtons.push({id: 2, content: "Spazieren gehen"}, {id: 3, content: "Zuhause bleiben"});
             }
-            else if (id == 2) {
-                allButtons.push({id: 4, content: "option4"}, {id: 5, content: "option5"});
+            else if (id == 2 || id == 9) {
+                allButtons.push({id: 4, content: "Auf dem Weg bleiben"}, {id: 5, content: "Rascheln nachgehen"});
+            }
+            else if (id == 3) {
+                allButtons.push({id: 6, content: "Herbitten"}, {id: 7, content: "Nicht herbitten"});
+            }
+            else if (id == 7) {
+                allButtons.push({id: 8, content: "Zuhause bleiben"}, {id: 9, content: "Nachsehen"});
+            }
+            else if (id == 8) {
+                allButtons.push({id: 10, content: "Netflix Serie"}, {id: 11, content: "Disney+ Film"});
+            }
+            else if (id == 12) {
+                allButtons.push({id: 0, content: "Tee"}, {id: 1, content: "Apfelsaft"});
+            }
+            else {
+                allButtons.push({id: 12, content: "Restart"});
             }
         }
     }
