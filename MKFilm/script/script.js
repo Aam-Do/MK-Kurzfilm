041a48cb849df6d5091b72ff34e4965179298b56
Am.D <ameliedelloro@gmail.com>
2021-07-10 12:41:43 +0200

    mmh monke

--- MKFilm/script/script.js
+++ MKFilm/script/script.js
@@ -1,4 +1,7 @@
 var buttonDiv;
+var video;
+var p;
+var path = "https://www.youtube.com/embed/";
 var allButtons = [
     { id: 0, content: "Tee" },
     { id: 1, content: "Apfelsaft" }
@@ -6,11 +9,13 @@ var allButtons = [
 var buttons;
 window.addEventListener("load", function () { load(); });
 function load() {
+    p = document.querySelector("p");
     buttonDiv = document.querySelector("#buttonDiv");
+    video = document.querySelector("iframe");
     drawButtons();
-    console.log("hi");
 }
 function drawButtons() {
+    p.classList.remove("hidden");
     buttonDiv.innerHTML = "";
     var _loop_1 = function (i) {
         var button = allButtons[i];
@@ -31,34 +36,75 @@ function buttonClick(id) {
     for (var i = 0; i < allButtons.length; i++) {
         var button = allButtons[i];
         if (button.id == id) {
-            console.log("option " + id + " was clicked");
             allButtons.length = 0;
-            if (id == 0 || id == 1) {
-                allButtons.push({ id: 2, content: "Spazieren gehen" }, { id: 3, content: "Zuhause bleiben" });
-            }
-            else if (id == 2) {
-                allButtons.push({ id: 13, content: "Wasser kaufen" }, { id: 14, content: "Ohne Wasser spazieren gehen" });
-            }
-            else if (id == 13 || id == 14 || id == 9) {
-                allButtons.push({ id: 4, content: "Auf dem Weg bleiben" }, { id: 5, content: "Rascheln nachgehen" });
-            }
-            else if (id == 3) {
-                allButtons.push({ id: 6, content: "Herbitten" }, { id: 7, content: "Nicht herbitten" });
-            }
-            else if (id == 7) {
-                allButtons.push({ id: 8, content: "Zuhause bleiben" }, { id: 9, content: "Nachsehen" });
-            }
-            else if (id == 8) {
-                allButtons.push({ id: 10, content: "Netflix Serie" }, { id: 11, content: "Disney+ Film" });
-            }
-            else if (id == 12) {
-                allButtons.push({ id: 0, content: "Tee" }, { id: 1, content: "Apfelsaft" });
-            }
-            else {
-                allButtons.push({ id: 12, content: "Restart" });
+            video.src = "";
+            switch (id) {
+                case 0:
+                    video.src = path + "cBN70U5HecY";
+                    allButtons.push({ id: 2, content: "Spazieren gehen" }, { id: 3, content: "Zuhause bleiben" });
+                    break;
+                case 1:
+                    video.src = path + "hmi7RDRW_40";
+                    allButtons.push({ id: 2, content: "Spazieren gehen" }, { id: 3, content: "Zuhause bleiben" });
+                    break;
+                case 2:
+                    video.src = path + "wxsPKXPkFzE";
+                    allButtons.push({ id: 13, content: "Wasser kaufen" }, { id: 14, content: "Ohne Wasser spazieren gehen" });
+                    break;
+                case 3:
+                    video.src = path + "";
+                    allButtons.push({ id: 6, content: "Herbitten" }, { id: 7, content: "Nicht herbitten" });
+                    break;
+                case 4:
+                    video.src = path + "";
+                    allButtons.push({ id: 12, content: "Restart" });
+                    break;
+                case 5:
+                    video.src = path + "";
+                    allButtons.push({ id: 12, content: "Restart" });
+                    break;
+                case 6:
+                    video.src = path + "fL3Oc4qhTe0";
+                    allButtons.push({ id: 12, content: "Restart" });
+                    break;
+                case 7:
+                    video.src = path + "KjA6bfcS-YA";
+                    allButtons.push({ id: 8, content: "Zuhause bleiben" }, { id: 9, content: "Nachsehen" });
+                    break;
+                case 8:
+                    video.src = path + "ixaohY7pZFI";
+                    allButtons.push({ id: 10, content: "Netflix gucken" }, { id: 11, content: "Amazon prime gucken" });
+                    break;
+                case 9:
+                    video.src = path + "";
+                    allButtons.push({ id: 4, content: "Auf dem Weg bleiben" }, { id: 5, content: "Rascheln nachgehen" });
+                    break;
+                case 10:
+                    video.src = path + "";
+                    allButtons.push({ id: 12, content: "Restart" });
+                    break;
+                case 11:
+                    video.src = path + "";
+                    allButtons.push({ id: 12, content: "Restart" });
+                    break;
+                case 12:
+                    video.src = path + "I4KVu5zdZiI";
+                    allButtons.push({ id: 0, content: "Tee" }, { id: 1, content: "Apfelsaft" });
+                    break;
+                case 13:
+                    video.src = path + "";
+                    allButtons.push({ id: 4, content: "Auf dem Weg bleiben" }, { id: 5, content: "Rascheln nachgehen" });
+                    break;
+                case 14:
+                    video.src = path + "";
+                    allButtons.push({ id: 4, content: "Auf dem Weg bleiben" }, { id: 5, content: "Rascheln nachgehen" });
+                    break;
             }
         }
     }
     drawButtons();
+}
+function hide() {
+    p.classList.add("hidden");
 }
 //# sourceMappingURL=script.js.map
--- MKFilm/script/script.ts
+++ MKFilm/script/script.ts
@@ -1,4 +1,7 @@
 var buttonDiv: HTMLElement;
+let video: HTMLIFrameElement;
+let p: HTMLParagraphElement;
+let path: string = "https://www.youtube.com/embed/";
 interface OptionButtons { id: number; content: string; }
 let allButtons: OptionButtons[] = [
     {id: 0, content: "Tee"},
@@ -8,11 +11,13 @@ var buttons: HTMLElement;
 window.addEventListener("load", function(): void { load(); });
 
 function load(): void {
+    p = document.querySelector("p");
     buttonDiv = document.querySelector("#buttonDiv");
+    video = document.querySelector("iframe");
     drawButtons();
-    console.log("hi");
 }
 function drawButtons(): void {
+    p.classList.remove("hidden");
     buttonDiv.innerHTML = "";
 
     for (let i: number = 0; i < allButtons.length; i++) {
@@ -35,36 +40,75 @@ function drawButtons(): void {
 function buttonClick (id: number): void {
     for (let i: number = 0; i < allButtons.length; i++) {
         let button: OptionButtons = allButtons[i];
-        
         if (button.id == id) {
-            console.log("option " + id + " was clicked");
             allButtons.length = 0;
-            if (id == 0 || id == 1) {
-                allButtons.push({id: 2, content: "Spazieren gehen"}, {id: 3, content: "Zuhause bleiben"});
-            }
-            else if (id == 2) {
-                allButtons.push({id: 13, content: "Wasser kaufen"}, {id: 14, content: "Ohne Wasser spazieren gehen"});
-            }
-            else if (id == 13 || id == 14 || id == 9) {
-                allButtons.push({id: 4, content: "Auf dem Weg bleiben"}, {id: 5, content: "Rascheln nachgehen"});
-            }
-            else if (id == 3) {
-                allButtons.push({id: 6, content: "Herbitten"}, {id: 7, content: "Nicht herbitten"});
-            }
-            else if (id == 7) {
-                allButtons.push({id: 8, content: "Zuhause bleiben"}, {id: 9, content: "Nachsehen"});
-            }
-            else if (id == 8) {
-                allButtons.push({id: 10, content: "Netflix Serie"}, {id: 11, content: "Disney+ Film"});
-            }
-            else if (id == 12) {
-                allButtons.push({id: 0, content: "Tee"}, {id: 1, content: "Apfelsaft"});
-            }
-            else {
-                allButtons.push({id: 12, content: "Restart"});
+            video.src = "";
+            switch (id) {
+                case 0:
+                    video.src = path + "cBN70U5HecY";
+                    allButtons.push({id: 2, content: "Spazieren gehen"}, {id: 3, content: "Zuhause bleiben"});
+                    break;
+                case 1:
+                    video.src = path + "hmi7RDRW_40";
+                    allButtons.push({id: 2, content: "Spazieren gehen"}, {id: 3, content: "Zuhause bleiben"});
+                    break;
+                case 2:
+                    video.src = path + "wxsPKXPkFzE";
+                    allButtons.push({id: 13, content: "Wasser kaufen"}, {id: 14, content: "Ohne Wasser spazieren gehen"});
+                    break;
+                case 3:
+                    video.src = path + "";
+                    allButtons.push({id: 6, content: "Herbitten"}, {id: 7, content: "Nicht herbitten"});
+                    break;
+                case 4:
+                    video.src = path + "";
+                    allButtons.push({id: 12, content: "Restart"});
+                    break;
+                case 5:
+                    video.src = path + "";
+                    allButtons.push({id: 12, content: "Restart"});
+                    break;
+                case 6:
+                    video.src = path + "fL3Oc4qhTe0";
+                    allButtons.push({id: 12, content: "Restart"});
+                    break;
+                case 7:
+                    video.src = path + "KjA6bfcS-YA";
+                    allButtons.push({id: 8, content: "Zuhause bleiben"}, {id: 9, content: "Nachsehen"});
+                    break;
+                case 8:
+                    video.src = path + "ixaohY7pZFI";
+                    allButtons.push({id: 10, content: "Netflix gucken"}, {id: 11, content: "Amazon prime gucken"});
+                    break;
+                case 9:
+                    video.src = path + "";
+                    allButtons.push({id: 4, content: "Auf dem Weg bleiben"}, {id: 5, content: "Rascheln nachgehen"});
+                    break;
+                case 10:
+                    video.src = path + "";
+                    allButtons.push({id: 12, content: "Restart"});
+                    break;
+                case 11:
+                    video.src = path + "";
+                    allButtons.push({id: 12, content: "Restart"});
+                    break;
+                case 12:
+                    video.src = path + "I4KVu5zdZiI";
+                    allButtons.push({id: 0, content: "Tee"}, {id: 1, content: "Apfelsaft"});
+                    break;
+                case 13:
+                    video.src = path + "";
+                    allButtons.push({id: 4, content: "Auf dem Weg bleiben"}, {id: 5, content: "Rascheln nachgehen"});
+                    break;
+                case 14:
+                    video.src = path + "";
+                    allButtons.push({id: 4, content: "Auf dem Weg bleiben"}, {id: 5, content: "Rascheln nachgehen"});
+                    break;
             }
         }
     }
     drawButtons();
 }
-
+function hide(): void {
+    p.classList.add("hidden");
+}
